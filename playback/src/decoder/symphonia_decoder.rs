36dad7fc7df30be8b99b374469b3e9d3066eaf81
Emil <40684650+MrCheatEugene@users.noreply.github.com>
2026-01-02 04:41:09 +0300

    Update symphonia_decoder.rs

--- playback/src/decoder/symphonia_decoder.rs
+++ playback/src/decoder/symphonia_decoder.rs
@@ -31,6 +31,131 @@ pub(crate) struct LocalFileMetadata {
     pub disc_number: Option<u32>,
 }
 
+pub struct SymphoniaDecoder_AF {
+    format: Box<dyn FormatReader>,
+    decoder: Box<dyn Decoder>,
+    sample_buffer: Option<SampleBuffer<f64>>,
+}
+
+impl SymphoniaDecoder_AF {
+    pub fn new<R>(input: R, file_format: AudioFileFormat) -> DecoderResult<Self>
+    where
+        R: MediaSource + 'static,
+    {
+        let mss_opts = MediaSourceStreamOptions {
+            buffer_len: librespot_audio::AudioFetchParams::get().minimum_download_size,
+        };
+        let mss = MediaSourceStream::new(Box::new(input), mss_opts);
+
+        let format_opts = FormatOptions {
+            enable_gapless: true,
+            ..Default::default()
+        };
+
+        let format: Box<dyn FormatReader> = if AudioFiles::is_ogg_vorbis(file_format) {
+            Box::new(OggReader::try_new(mss, &format_opts)?)
+        } else if AudioFiles::is_mp3(file_format) {
+            Box::new(MpaReader::try_new(mss, &format_opts)?)
+        } else if AudioFiles::is_flac(file_format) {
+            Box::new(FlacReader::try_new(mss, &format_opts)?)
+        } else {
+            return Err(DecoderError::SymphoniaDecoder(format!(
+                "Unsupported format: {file_format:?}"
+            )));
+        };
+
+        let track = format.default_track().ok_or_else(|| {
+            DecoderError::SymphoniaDecoder("Could not retrieve default track".into())
+        })?;
+
+        let decoder_opts: DecoderOptions = Default::default();
+        let decoder: Box<dyn Decoder> = if AudioFiles::is_ogg_vorbis(file_format) {
+            Box::new(VorbisDecoder::try_new(&track.codec_params, &decoder_opts)?)
+        } else if AudioFiles::is_mp3(file_format) {
+            Box::new(MpaDecoder::try_new(&track.codec_params, &decoder_opts)?)
+        } else if AudioFiles::is_flac(file_format) {
+            Box::new(FlacDecoder::try_new(&track.codec_params, &decoder_opts)?)
+        } else {
+            return Err(DecoderError::SymphoniaDecoder(format!(
+                "Unsupported decoder: {file_format:?}"
+            )));
+        };
+
+        let rate = decoder.codec_params().sample_rate.ok_or_else(|| {
+            DecoderError::SymphoniaDecoder("Could not retrieve sample rate".into())
+        })?;
+        if rate != SAMPLE_RATE {
+            return Err(DecoderError::SymphoniaDecoder(format!(
+                "Unsupported sample rate: {rate}"
+            )));
+        }
+
+        let channels = decoder.codec_params().channels.ok_or_else(|| {
+            DecoderError::SymphoniaDecoder("Could not retrieve channel configuration".into())
+        })?;
+        if channels.count() != NUM_CHANNELS as usize {
+            return Err(DecoderError::SymphoniaDecoder(format!(
+                "Unsupported number of channels: {channels}"
+            )));
+        }
+
+        Ok(Self {
+            format,
+            decoder,
+
+            // We set the sample buffer when decoding the first full packet,
+            // whose duration is also the ideal sample buffer size.
+            sample_buffer: None,
+        })
+    }
+
+    pub fn normalisation_data(&mut self) -> Option<NormalisationData> {
+        let mut metadata = self.format.metadata();
+
+        // Advance to the latest metadata revision.
+        // None means we hit the latest.
+        loop {
+            if metadata.pop().is_none() {
+                break;
+            }
+        }
+
+        let tags = metadata.current()?.tags();
+
+        if tags.is_empty() {
+            None
+        } else {
+            let mut data = NormalisationData::default();
+
+            for tag in tags {
+                if let Value::Float(value) = tag.value {
+                    match tag.std_key {
+                        Some(StandardTagKey::ReplayGainAlbumGain) => data.album_gain_db = value,
+                        Some(StandardTagKey::ReplayGainAlbumPeak) => data.album_peak = value,
+                        Some(StandardTagKey::ReplayGainTrackGain) => data.track_gain_db = value,
+                        Some(StandardTagKey::ReplayGainTrackPeak) => data.track_peak = value,
+                        _ => (),
+                    }
+                }
+            }
+
+            Some(data)
+        }
+    }
+
+    #[inline]
+    fn ts_to_ms(&self, ts: u64) -> u32 {
+        match self.decoder.codec_params().time_base {
+            Some(time_base) => {
+                let time = Duration::from(time_base.calc_time(ts));
+                time.as_millis() as u32
+            }
+            // Fallback in the unexpected case that the format has no base time set.
+            None => (ts as f64 * PAGES_PER_MS) as u32,
+        }
+    }
+}
+
 impl SymphoniaDecoder {
     pub fn new<R>(input: R, hint: Hint) -> DecoderResult<Self>
     where
