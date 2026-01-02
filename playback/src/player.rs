83a55fb086c35fb7c23eb25d56f0827cf5ed1b94
Emil <40684650+MrCheatEugene@users.noreply.github.com>
2026-01-02 04:41:32 +0300

    Update player.rs

--- playback/src/player.rs
+++ playback/src/player.rs
@@ -24,7 +24,7 @@ use crate::{
     config::{Bitrate, NormalisationMethod, NormalisationType, PlayerConfig},
     convert::Converter,
     core::{Error, Session, SpotifyId, SpotifyUri, util::SeqGenerator},
-    decoder::{AudioDecoder, AudioPacket, AudioPacketPosition, SymphoniaDecoder},
+    decoder::{AudioDecoder, AudioPacket, AudioPacketPosition, SymphoniaDecoder_AF},
     metadata::audio::{AudioFileFormat, AudioFiles, AudioItem},
     mixer::VolumeGetter,
 };
@@ -1098,7 +1098,7 @@ impl PlayerTrackLoader {
             };
 
             let mut symphonia_decoder = |audio_file, format| {
-                SymphoniaDecoder::new(audio_file, format).map(|mut decoder| {
+                SymphoniaDecoder_AF::new(audio_file, format).map(|mut decoder| {
                     // For formats other that Vorbis, we'll try getting normalisation data from
                     // ReplayGain metadata fields, if present.
                     if normalisation_data.is_none() {
